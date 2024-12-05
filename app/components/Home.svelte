8720d6d97db29359b1003a25fcc31f2cfe87fea8
CodyMarkix <marekplasek08@protonmail.com>
2024-12-05 06:22:05 +0100

    basic layout building

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,21 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
+<page actionBarHidden="true">
+    <gridLayout rows="100, 100, 100, 100, 100, 100, 100, 100" columns="100*, 100*, 100*">
+        <label text="0,1" row="0" col="0" backgroundColor="#1c486b"/>
+        <!--<image src="~/assets/logo.png" row="0" col="1" style="margin: 2%"/> -->
+        <image src="~/assets/placeholderAccount.jpg" row="0" col="2" style="margin: 3%"/>
     </gridLayout>
+
 </page>
 
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+    import { onMount } from "svelte";
+    let message: string = "Blank Svelte Native App";
+
+    onMount(() => {
+        console.log("Hello world!")
+    });
 </script>
 
 <style>
-    .info .fas {
-        color: #3A53FF;
-    }
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
 </style>
